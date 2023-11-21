3f0d7be2079554dae9200160158c959270533929
Berenekerr <151638189+Berenekerr@users.noreply.github.com>
2023-11-21 16:07:56 +0300

    Calculating age

---
+++ ceng105 .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int age,birthyear;
+	printf("Enter your birthyear :");
+	scanf("%d",&birthyear);
+	
+	age=2023-birthyear;
+	printf("your age is: %d",age);
+	
+	return(0);
+	
+	
+	
+}
